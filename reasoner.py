ba3a96065f094826ce30401cef36340ca5cc2da0
Oskar Wang <80847085+owhygithub@users.noreply.github.com>
2024-12-09 15:46:05 +0100

    EL algorithm implemented
    EL algorithm implemented

--- reasoner.py
+++ reasoner.py
@@ -1,48 +1,150 @@
-#! /usr/bin/python3
+#! /usr/bin/python
 
-import sys
 from py4j.java_gateway import JavaGateway
 
-def main():
-    if len(sys.argv) != 3:
-        print("Usage: python reasoner.py DumplingOntology.owx Dumpling")
-        sys.exit(1)
+# connect to the java gateway of dl4python
+gateway = JavaGateway()
 
-    ontology_file = sys.argv[1]
-    class_name = sys.argv[2]
+# get a parser from OWL files to DL ontologies
+parser = gateway.getOWLParser()
 
-    # Connect to the Java gateway of dl4python
-    gateway = JavaGateway()
+# get a formatter to print in nice DL format
+formatter = gateway.getSimpleDLFormatter()
 
-    # Get the parser and formatter from dl4python
-    parser = gateway.getOWLParser()
-    formatter = gateway.getSimpleDLFormatter()
+print("Loading the ontology...")
 
-    try:
-        # Load the ontology
-        ontology = parser.parseFile(ontology_file)
+# load an ontology from a file
+ontology = parser.parseFile("dumplings.owl")
 
-        # Convert to binary conjunctions as required
-        gateway.convertToBinaryConjunctions(ontology)
+print("Loaded the ontology!")
 
-        # Initialize the EL reasoner
-        elk = gateway.getELKReasoner()
-        elk.setOntology(ontology)
+# IMPORTANT: the algorithm from the lecture assumes conjunctions to always be over two concepts
+# Ontologies in OWL can however have conjunctions over an arbitrary number of concpets.
+# The following command changes all conjunctions so that they have at most two conjuncts
+print("Converting to binary conjunctions")
+gateway.convertToBinaryConjunctions(ontology)
 
-        # Get the EL factory to work with concepts
-        el_factory = gateway.getELFactory()
-        target_concept = el_factory.getConceptName(class_name)
+# get the TBox axioms
+tbox = ontology.tbox()
+axioms = tbox.getAxioms()
 
-        # Compute subsumers for the given class
-        subsumers = elk.getSubsumers(target_concept)
+print("These are the axioms in the TBox:")
+for axiom in axioms:
+    print(formatter.format(axiom))
 
-        # Print the subsumers, one per line
-        for concept in sorted(subsumers, key=lambda x: formatter.format(x)):
-            print(formatter.format(concept))
+# get all concepts occurring in the ontology
+allConcepts = ontology.getSubConcepts()
 
-    except Exception as e:
-        print(f"Error: {e}", file=sys.stderr)
-        sys.exit(1)
+print("There are ", len(allConcepts), " concepts occurring in the ontology")
+print("These are the concepts occurring in the ontology:")
+print([formatter.format(x) for x in allConcepts])
 
-if __name__ == "__main__":
-    main()
+conceptNames = ontology.getConceptNames()
+
+print("There are ", len(conceptNames), " concept names occurring in the ontology")
+print("These are the concept names: ")
+print([formatter.format(x) for x in conceptNames])
+
+# access the type of axioms:
+foundGCI = False
+foundEquivalenceAxiom = False
+print("Looking for axiom types in EL")
+for axiom in axioms:
+    axiomType = axiom.getClass().getSimpleName()
+    if(not(foundGCI) and axiomType == "GeneralConceptInclusion"):
+        print("I found a general concept inclusion:")
+        print(formatter.format(axiom))
+        print("The left hand side of the axiom is: ", formatter.format(axiom.lhs()))
+        print("The right hand side of the axiom is: ", formatter.format(axiom.rhs()))
+        print()
+        foundGCI = True
+    elif(not(foundEquivalenceAxiom) and axiomType == "EquivalenceAxiom"):
+        print("I found an equivalence axiom:")
+        print(formatter.format(axiom))
+        print("The concepts made equivalent are: ")
+        for concept in axiom.getConcepts():
+            print(" - "+formatter.format(concept))
+        foundEquivalenceAxiom = True
+
+# accessing the relevant types of concepts:
+foundConceptName=False
+foundTop=False
+foundExistential=False
+foundConjunction=False
+foundConceptTypes = set()
+
+print("Looking for concept types in EL")
+for concept in allConcepts:
+    conceptType = concept.getClass().getSimpleName()
+    if(not(conceptType in foundConceptTypes)): 
+        print(conceptType)
+        foundConceptTypes.add(conceptType)
+    if(not(foundConceptName) and conceptType == "ConceptName"):
+        print("I found a concept name: "+formatter.format(concept))
+        print()
+        foundConceptName = True
+    elif(not(foundTop) and conceptType == "TopConcept$"):
+        print("I found the top concept: "+formatter.format(concept))
+        print()
+        foundTop = True
+    elif(not(foundExistential) and conceptType == "ExistentialRoleRestriction"):
+        print("I found an existential role restriction: "+formatter.format(concept))
+        print("The role is: "+formatter.format(concept.role()))
+        print("The filler is: "+formatter.format(concept.filler()))
+        print()
+        foundExistential = True
+    elif(not(foundConjunction) and conceptType == "ConceptConjunction"):
+        print("I found a conjunction: "+formatter.format(concept))
+        print("The conjuncts are: ")
+        for conjunct in concept.getConjuncts():
+            print(" - "+formatter.format(conjunct))
+        print()
+        foundConjunction=True
+
+# Creating EL concepts and axioms
+
+elFactory = gateway.getELFactory()
+
+conceptA = elFactory.getConceptName("A")
+conceptB = elFactory.getConceptName("B")
+conjunctionAB = elFactory.getConjunction(conceptA, conceptB)
+role = elFactory.getRole("r")
+existential = elFactory.getExistentialRoleRestriction(role, conjunctionAB)
+top = elFactory.getTop()
+conjunction2 = elFactory.getConjunction(top, existential)
+
+gci = elFactory.getGCI(conjunctionAB, conjunction2)
+
+print("I made the following GCI:")
+print(formatter.format(gci))
+
+# Using the reasoners
+
+elk = gateway.getELKReasoner()
+hermit = gateway.getHermiTReasoner() # might the upper case T!
+
+dumpling_concept = elFactory.getConceptName("dumplings:Dumpling")
+
+def print_results(reasoner, concept):
+    print(f"\nResults from {reasoner.__class__.__name__}:")
+    reasoner.setOntology(ontology)
+
+    # Get the subsumers for the given concept
+    subsumers = reasoner.getSubsumers(concept)
+    print(f"According to {reasoner.__class__.__name__}, {formatter.format(concept)} has the following subsumers:")
+    for subsumer in subsumers:
+        print(" - ", formatter.format(subsumer))
+    print(f"({len(subsumers)} in total)")
+
+    # Classify the ontology and print the result
+    classificationResult = reasoner.classify()
+    print("Classification result: (too large to display)")
+
+# Option to print classification and subsumer results
+should_print = True  # Set to False to skip printing
+
+if should_print:
+    print_results(elk, dumpling_concept)
+    print_results(hermit, dumpling_concept)
+else:
+    print("Results will not be printed.")
